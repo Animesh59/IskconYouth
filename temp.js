d756de4852e6a6d1da43ce249b17aef3a5c5f993
Animesh59 <abelieve0304@gmail.com>
2023-11-08 21:17:43 +0530

    toggle of function implementated

--- temp.js
+++ temp.js
@@ -1,10 +1,20 @@
 var btn = document.getElementById('btn');
-
+var search = "all"
 
 btn.addEventListener('click', () => {
-    console.log('clicked');
-    input.removeEventListener('keyup', filter_All);
-    input.addEventListener('keyup', filter_DOB);
-    input.placeholder = "Search into DOB..."
-    filter_DOB();
+    if (search === "all") {
+        console.log('DOB seach actived');
+        input.removeEventListener('keyup', filter_All);
+        input.addEventListener('keyup', filter_DOB);
+        input.placeholder = "Search into DOB..."
+        search = "DOB"
+        filter_DOB();
+    } else {
+        console.log('All seach actived');
+        input.removeEventListener('keyup', filter_DOB);
+        input.addEventListener('keyup', filter_All);
+        input.placeholder = "Search into whole table..."
+        search = "all";
+        filter_All();
+    }
 })
